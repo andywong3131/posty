3a241d9ecbf1dfd7423a7626ed2fb2833329b04f
andywong31 <wongandy31@gmail.com>
2021-06-24 06:21:18 +0800

    created initial web app layout and downloaded some development packages

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="flex justify-center">
+  <div class="w-6/12 bg-white p-5 rounded-lg">
+    <h1 class="mb-5 font-bold">Home</h1>
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis enim vero a praesentium qui excepturi ad, eligendi officiis quae magnam quam nisi, ducimus impedit error in debitis perspiciatis culpa ipsam!Dolorem facere reprehenderit non architecto, magni sequi autem minima qui error? Incidunt dolore animi iste eos temporibus cum deserunt numquam, laboriosam aspernatur facilis consequatur dolorem itaque repudiandae fugiat commodi? Laboriosam?</p>
+  </div>
+</div>
+@endsection
---
+++ resources/views/partials/_navbar.blade.php
@@ -0,0 +1,14 @@
+<nav class="bg-black flex justify-between">
+  <ul class="flex text-gray-300">
+    <li class="m-5"><a href="">Home</a></li>
+    <li class="m-5"><a href="">Dashboard</a></li>
+    <li class="m-5"><a href="">Posts</a></li>
+  </ul>
+
+  <ul class="flex text-gray-300">
+    <li class="m-5"><a href="">Login</a></li>
+    <li class="m-5"><a href="">Register</a></li>
+    <li class="m-5"><a href="">Andy</a></li>
+    <li class="m-5"><a href="">Logout</a></li>
+  </ul>
+</nav>
